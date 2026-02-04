962b6697aa96371a5235c0fe32e9d7eeb7181d3d
GuiXvr <vr0131902@gmail.com>
2026-02-03 21:02:52 -0300

    Planejamento-Do-Codigo-Da-Questao-2
    Fiz a lógica do que deve ser feito nessa questão, falta conferir e desenvolver o código.

--- questao2.c
+++ questao2.c
@@ -1,1 +1,11 @@
-/*Teste*/
+/*Questão: 12) Considere 10 países a sua escolha. Crie um ranking dos países com base no número total de medalhas
+conquistadas. Ordene os países pelo total de medalhas
+
+LOGICA: 1- Escolhe 10 países;
+2-Conta total de medalhas de cada um deles e rankeia isso na impressao;
+3- Ordena países de maneira decrescente do que possui maior numero de medalhas para o que contém menor numero e imprime;
+4- Gráfico (EXTRA).
+*/ 
+
+
+
