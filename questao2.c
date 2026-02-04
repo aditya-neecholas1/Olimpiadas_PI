42ee25c23a2146a78e7b16c4ab0da632916a5037
GuiXvr <vr0131902@gmail.com>
2026-02-03 22:35:43 -0300

    Desenvolvimento-Inicial-Questao2
    Fiz uma estrutura maior do que farei de maneira mais extensiva e detalhada em breve.

--- questao2.c
+++ questao2.c
@@ -7,5 +7,75 @@ LOGICA: 1- Escolhe 10 países;
 4- Gráfico (EXTRA).
 */ 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define MAX_LINE 2048
+#define MAX_PAISES 250
+#define MAX_NOC 4
+#define MAX_NOME_PAIS 100
 
+//Para armazenar os dados de madalha por país criei uma struct:
+typedef struct{
+char noc[MAX_NOC]; //Aqui declarei a variavel do codigo do país.
+char nomePais[MAX_NOME_PAIS]; // Aqui é a variavel para o nome completo do país.
+int ouro, prata, bronze;
+int total;
+} Pais;
+
+// Para mapear o NOC de acordo com o nome do país criei outra struct:
+typedef struct {
+    char noc[MAX_NOC];
+    char nome[MAX_NOME_PAIS];
+} mapeamentoNOC;
+
+/*
+.
+.
+.
+FALTA UMA FUNÇÃO PARA REMOVER ESPAÇOS EM BRANCO NO INICIO E NO FIM !!!!
+.
+.
+.
+*/
+
+// Para remover as aspas de uma string utilizei essa função:
+void removeAspas(char *str) {
+    char temporario[MAX_LINE];
+    int inteiro1 = 0, inteiro2 = 0;
+    
+    while(str[inteiro1]) {
+        if(str[inteiro1] != '"') {
+            temporario[inteiro2++] = str[inteiro2];
+        }
+        inteiro1++;
+    }
+    temporario[inteiro2] = '\0';
+    strcpy(str, temporario);
+}
+
+/*
+.
+.
+.
+IREI COLOCAR OUTRAS FUNÇÕES
+.
+.
+.
+*/
+
+int main(){
+    Pais paises[10];
+    //MapeamentoNOC mapeamento[MAX_PAISES]; // FUNCAO A SER CRIADA!!
+    int total_paises;
+    int total_mapeamento;
+
+    // Listando paises selecionados para utilizar 
+    const char *paises_selecionados[] = {
+        "BRA", "USA", "CHN", "GBR", "FRA", 
+        "GER", "JPN", "AUS", "ITA", "CAN"
+    };
+    int num_selecionados = 10;
+
+}
