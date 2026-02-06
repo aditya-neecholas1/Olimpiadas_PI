43e72e06cae7e0575564b09ec083958f66248562
Aditya <aditya.singh@dcomp.ufs.br>
2026-02-05 22:38:19 -0300

    Comecei a questao 2 e atualizei o main

--- main.c
+++ main.c
@@ -3,6 +3,46 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "questao1.h"
+#include "questao2.h"
+
+int main() {
+    int opcao;
+    while(true){
+        printf("MENU - INFORMAÇÕES SOBRE AS OLIMPÍADAS\n");
+        printf("Digite um dos numeros a seguir para utilizar o programa:\n");
+        printf("1 - Questao 1 - Ranking dos países por esporte\n");
+        printf("2 - Questao 2 - Ranking de 10 países por número de atletas\n");
+        printf("3 - Questao 3 - ?\n");
+        printf("4 - Questao 4 - ?\n");
+        printf("0 - Sair do programa\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        if(opcao == 1){
+            questao1exe();
+        } else if(opcao == 2){
+            questao2exe();
+        } else if(opcao == 3){
+            printf("Questao 3 ainda nao foi implementada.\n");
+            printf("Aperte Enter para voltar ao menu.");
+            limparBuffer();
+            getchar();
+        } else if(opcao == 4){
+            printf("Questao 4 ainda nao foi implementada.\n");
+            printf("Aperte Enter para voltar ao menu.");
+            limparBuffer();
+            getchar();
+        } else if(opcao == 0){
+            printf("Fim do programa. Encerrando...\n");
+            break;
+        } else{
+            printf("Opcao invalida. Tente novamente.\n");
+            limparBuffer();
+            getchar();
+        }
+
+    }
+    return 0;
+}
 
 void limparBuffer(){
     int limpeza;
@@ -31,44 +71,19 @@ void questao1exe(){
     printf("Pressione Enter para voltar.");
     getchar();
 }
-int main() {
-    int opcao;
-    while(true){
-        printf("MENU - INFORMAÇÕES SOBRE AS OLIMPÍADAS\n");
-        printf("Digite um dos numeros a seguir para utilizar o programa:\n");
-        printf("1 - Questao 1 - Ranking dos países por esporte\n");
-        printf("2 - Questao 2 - ?\n");
-        printf("3 - Questao 3 - ?\n");
-        printf("4 - Questao 4 - ?\n");
-        printf("0 - Sair do programa\n");
-        printf("Escolha uma opcao: ");
-        scanf("%d", &opcao);
-        if(opcao == 1){
-            questao1exe();
-        } else if(opcao == 2){
-            printf("Questao 2 ainda nao foi implementada.\n");
-            printf("Aperte Enter para voltar ao menu.");
-            limparBuffer();
-            getchar();
-        } else if(opcao == 3){
-            printf("Questao 3 ainda nao foi implementada.\n");
-            printf("Aperte Enter para voltar ao menu.");
-            limparBuffer();
-            getchar();
-        } else if(opcao == 4){
-            printf("Questao 4 ainda nao foi implementada.\n");
-            printf("Aperte Enter para voltar ao menu.");
-            limparBuffer();
-            getchar();
-        } else if(opcao == 0){
-            printf("Fim do programa. Encerrando...\n");
-            break;
-        } else{
-            printf("Opcao invalida. Tente novamente.\n");
-            limparBuffer();
-            getchar();
-        }
 
+void questao2exe(){
+    /*2ª questão: Considere 10 países a sua escolha. 
+    Crie uma lista de países ordenada pelo número total de atletas
+    cadastrados, do maior para o menor.*/
+    limparBuffer();
+    int quantidade;
+    PaisAtletas* paisesEscolhidos = criarPaises(&quantidade);
+    contarAtletasPorPais(paisesEscolhidos, 10);
+    printf("RANKING DOS 10 PAISES COM MAIS ATLETAS:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%dº - %s: %d atletas cadastrados\n", i + 1, (paisesEscolhidos + i)->noc, (paisesEscolhidos + i)->atletas);
     }
-    return 0;
+    printf("Pressione Enter para voltar.");
+    getchar();
 }
